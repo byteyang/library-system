3f05533fccc74fe61658e90cb7e66db6d2ca8b2e
杨子杰 <byteyang@gmail.com>
2024-04-11 17:47:50 +0800

    feat: use sqlite3 instead of mysql (#1)
    Co-authored-by: yangzijie <yangzijie@neusoftmedical.com>

--- server/src/database.js
+++ server/src/database.js
@@ -1,14 +1,9 @@
-const mysql = require('mysql2')
+const knex = require('knex')
 
-exports.pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 2,
-  queueLimit: 0,
-  enableKeepAlive: true,
-  keepAliveInitialDelay: 0,
-}).promise()
+exports.db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: './db.sqlite3',
+  },
+  useNullAsDefault: true,
+})
